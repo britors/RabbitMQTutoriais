239266ecc068c4df965bce8732453a80c779b7ac
Rodrigo Brito <britors@users.noreply.github.com>
2021-03-11 10:25:40 -0300

    feat: Colocar e Consumir arquivos pela Fila

---
+++ FileQueues/ProduceFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.IO;
+
+namespace ProduceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "file_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+
+                byte[] body = File.ReadAllBytes(@"..\..\..\File\Example.xlsx");
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "file_queue",
+                                     basicProperties: properties,
+                                     body: body);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
